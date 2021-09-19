f16c964053cb1db1f023d4c710c3ae587166b342
Luiz Gustavo <gustavo_x@live.com>
2021-09-19 18:22:45 -0300

    inital commit

---
+++ src/test/java/com/fatec/sasbackend/SasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.sasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
